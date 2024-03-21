bd1311bf57c828c23a99cfc71e4690b1f8a90bee
Jeff Bazinet <139579570+jeff-bazinet@users.noreply.github.com>
2024-03-21 15:58:34 -0400

    Update styles for book tile

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,19 @@
 const myLibrary = [];
 
 function Book(
-  author,
   title,
+  author,
   pages,
   publicationDate,
   publisher,
   isbn10,
   isbn13,
   language,
-  readStatus
+  readStatus,
+  thumbnail
 ) {
-  this.author = author;
   this.title = title;
+  this.author = author;
   this.pages = pages;
   this.publicationDate = publicationDate;
   this.publisher = publisher;
@@ -20,36 +21,82 @@ function Book(
   this.isbn13 = isbn13;
   this.language = language;
   this.readStatus = readStatus;
+  this.thumbnail = thumbnail;
 }
 
 function addBookToLibrary(
-  author,
   title,
+  author,
   pages,
   publicationDate,
   publisher,
   isbn10,
   isbn13,
   language,
-  readStatus
+  readStatus,
+  thumbnail
 ) {
   const newBook = new Book(
-    author,
     title,
+    author,
     pages,
     publicationDate,
     publisher,
     isbn10,
     isbn13,
     language,
-    readStatus
+    readStatus,
+    thumbnail
   );
   myLibrary.push(newBook);
 }
 
 function displayLibraryBooks() {
+  const mainGrid = document.querySelector('.book-grid');
+
   myLibrary.forEach((book) => {
-    console.log(book);
+    const newBook = document.createElement('div');
+    const newBookDetails = document.createElement('div');
+    const newBookThumbnail = document.createElement('div');
+
+    newBook.classList.add('book');
+    newBookDetails.classList.add('book-details');
+    newBookThumbnail.classList.add('book-thumbnail');
+
+    const bookDetails = [
+      { label: 'Title: ', value: book.title },
+      { label: 'Author: ', value: book.author },
+      { label: 'Pages: ', value: book.pages },
+      { label: 'Publication Date: ', value: book.publicationDate },
+      { label: 'Publisher: ', value: book.publisher },
+      { label: 'ISBN-10: ', value: book.isbn10 },
+      { label: 'ISBN-13: ', value: book.isbn13 },
+      { label: 'Language: ', value: book.language },
+      { label: 'Have Read: ', value: book.readStatus },
+    ];
+
+    bookDetails.forEach((detail) => {
+      const detailElement = document.createElement('p');
+      const detailElementLabel = document.createElement('span');
+
+      detailElementLabel.classList.add('book-label');
+      detailElementLabel.innerText = detail.label;
+      detailElement.appendChild(detailElementLabel);
+
+      const detailElementValue = document.createTextNode(detail.value);
+      detailElement.appendChild(detailElementValue)
+
+      console.log(detailElement.innerText)
+      newBookDetails.appendChild(detailElement);
+    });
+
+    const bookThumbnail = document.createElement('img');
+    bookThumbnail.src = book.thumbnail;
+    newBookThumbnail.appendChild(bookThumbnail);
+
+    newBook.appendChild(newBookDetails);
+    newBook.appendChild(newBookThumbnail);
+    mainGrid.appendChild(newBook);
   });
 }
 
@@ -62,7 +109,8 @@ addBookToLibrary(
   '9780141439518',
   '978-0141439518',
   'en',
-  false
+  false,
+  'images/pride-and-prejudice.jpg'
 );
 
 addBookToLibrary(
